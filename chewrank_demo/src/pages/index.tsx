2e62916af08374b3443021bbe0c48c93e857de83
evanchen51 <evanchen51@gmail.com>
2023-05-22 22:16:19 -0700

    [Test] Resize

--- chewrank_demo/src/pages/index.tsx
+++ chewrank_demo/src/pages/index.tsx
@@ -26,52 +26,56 @@ const Index: NextPage = () => {
       {/* <div className="absolute bottom-[-50px] right-4 z-[0]">
         <Image src="Logo2.png" width={300} height={300} alt="Nav" className="" />
       </div> */}
-      <div
-        id="target"
-        // className="relative flex h-[720px] w-[360px] flex-col overflow-hidden rounded-[20px] bg-white"
-        className="relative flex h-[720px] w-[360px] flex-col overflow-hidden rounded-[20px] bg-white shadow-[0_25px_50px_-12px_rgb(179_43_0_/_0.5)]"
-      >
-        <Restaurant page={page} setPage={setPage} setComing={setComing} />
-        <Review
-          page={page}
-          setPage={setPage}
-          setComing={setComing}
-          setSeed={setSeed}
-          seed={seed}
-        />
-        <Home setPage={setPage} setComing={setComing} setSeed={setSeed} />
+      <div className="scale-90">
         <div
-          className={
-            "absolute inset-x-0 bottom-0 z-[9999] h-20 w-[360px] rounded-b-[20px] border-t border-[#FFF6F3] bg-white"
-          }
+          id="target"
+          // className="relative flex h-[720px] w-[360px] flex-col overflow-hidden rounded-[20px] bg-white"
+          className="relative flex h-[720px] w-[360px] flex-col overflow-hidden rounded-[20px] bg-white shadow-[0_25px_50px_-12px_rgb(179_43_0_/_0.5)]"
         >
-          <Image
-            src="Nav.png"
-            fill={true}
-            alt="Nav"
-            className="pb-[4px]"
-            onClick={(e) => {
-              e.stopPropagation();
-              setComing((p) => (p[0] === 0 ? [e.clientX, e.clientY] : [0, 0]));
-            }}
+          <Restaurant page={page} setPage={setPage} setComing={setComing} />
+          <Review
+            page={page}
+            setPage={setPage}
+            setComing={setComing}
+            setSeed={setSeed}
+            seed={seed}
           />
+          <Home setPage={setPage} setComing={setComing} setSeed={setSeed} />
+          <div
+            className={
+              "absolute inset-x-0 bottom-0 z-[9999] h-20 w-[360px] rounded-b-[20px] border-t border-[#FFF6F3] bg-white"
+            }
+          >
+            <Image
+              src="Nav.png"
+              fill={true}
+              alt="Nav"
+              className="pb-[4px]"
+              onClick={(e) => {
+                e.stopPropagation();
+                setComing((p) =>
+                  p[0] === 0 ? [e.clientX, e.clientY] : [0, 0]
+                );
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <div
-        className="absolute z-[9999] origin-bottom ease-in-out"
-        style={{
-          left: (coming[0] || 0) - 60,
-          top: (coming[1] || 0) - 60,
-        }}
-      >
         <div
-          className="relative duration-300"
+          className="absolute z-[9999] origin-bottom ease-in-out"
           style={{
-            filter: "drop-shadow(0px 2px 12px rgba(179,43,0,0.9))",
-            transform: coming[0] === 0 ? "scale(0)" : "scale(1)",
+            left: (coming[0] || 0) - 60,
+            top: (coming[1] || 0) - 60,
           }}
         >
-          <Image src="ComingSoon.png" width={120} height={40} alt="Tooltip" />
+          <div
+            className="relative duration-300"
+            style={{
+              filter: "drop-shadow(0px 2px 12px rgba(179,43,0,0.9))",
+              transform: coming[0] === 0 ? "scale(0)" : "scale(1)",
+            }}
+          >
+            <Image src="ComingSoon.png" width={120} height={40} alt="Tooltip" />
+          </div>
         </div>
       </div>
     </div>
